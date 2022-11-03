752e3ba3fc79194fd835e78eb05a21084a723517
OLUWASEGUN MOSES OLOPADE <mose14real@gmail.com>
2022-11-03 21:57:49 +0100

    fix errors and content update

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # myPorfolioWithNodeJS
 
 => Create a portfolio having the following HTML pages:
-
 1. Home: The homepage should contain your data (bio).
 
 2. About: The about me page should contain brief information about you.
@@ -12,7 +11,6 @@
 => Create routes that render the HTML pages you created on a browser.
 => The base URL of your app should render the homepage.
 => There should be a /home route that redirects to the base of your app.
-=> Push your code to GitHub.
 
 Note: 1. No third-party dependencies
-      2. You don’t need to style your HTML pages
+      2. Styling your HTML pages is not compulsory
--- server.js
+++ server.js
@@ -1,45 +1,45 @@
 const http = require('http');
-const port = 3000;
+const port = 5000;
 const hostname = '127.0.0.1';
 const fs = require('fs');
 
-const server = http.createServer((request, response) => {
-    if (request.method === 'GET') {
-        switch (request.url) {
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET') {
+        switch (req.url) {
             case "/": {
                 fs.readFile('home.html', (err, data) => {
                     if (err) throw err;
                     const readableData = String(data);
-                    response.writeHead(200, { 'Content-Type': 'text/html' });
-                    response.write(readableData);
-                    response.end();
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(readableData);
+                    res.end();
                 })
             }
             case "/home": {
                 fs.readFile('home.html', (err, data) => {
                     if (err) throw err;
                     const readableData = String(data);
-                    response.writeHead(200, { 'Content-Type': 'text/html' });
-                    response.write(readableData);
-                    response.end();
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(readableData);
+                    res.end();
                 })
             }
             case "/about": {
                 fs.readFile('about.html', (err, data) => {
                     if (err) throw err;
                     const readableData = String(data);
-                    response.writeHead(200, { 'Content-Type': 'text/html' });
-                    response.write(readableData);
-                    response.end();
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(readableData);
+                    res.end();
                 })
             }
             case "/contact": {
                 fs.readFile('contact.html', (err, data) => {
                     if (err) throw err;
                     const readableData = String(data);
-                    response.writeHead(200, { 'Content-Type': 'text/html' });
-                    response.write(readableData);
-                    response.end();
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(readableData);
+                    res.end();
                 })
             }
         }
